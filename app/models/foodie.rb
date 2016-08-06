211c5a8a69ff5024d731c55c77037b1bda9e432c
raulescu <raul.leul@yahoo.com>
2016-08-06 15:46:37 +0200

    add validations to foodie/meal

--- app/models/foodie.rb
+++ app/models/foodie.rb
@@ -1,2 +1,4 @@
 class Foodie < ApplicationRecord
+  validates :name, presence: true
+  validates :content, presence: true
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,4 @@
 class Meal < ApplicationRecord
+  validations :name, presence: true
+  validations :image_url, length: { maximum: 250 }, allow_blank: true
 end
