42605eb3ca0e5d81a7e38ec61f615726e3cc0d2f
Veravg <veravgelder@hotmail.com>
2016-08-06 16:15:08 +0200

    Add seeds

--- app/models/foodie.rb
+++ app/models/foodie.rb
@@ -1,2 +1,5 @@
 class Foodie < ApplicationRecord
+  def self.order_by_name
+    order(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+foodies = Foodie.create( { name: "soup coriander", content: "  1 tablespoon kosher salt, plus more to tastefrom heat.", image_url: "http://images.media-allrecipes.com/userphotos/720x405/2610565.jpg" } )
+foodies2 = Foodie.create( { name: "Roasted sweet potato & carrot soup", content: "500g sweerl of crepper.", image_url: "http://images.dinosaurpictures.org.rsz.io/nyito_4f23.jpg" } )
+
+
+foodies3 = Foodie.create( { name: "Roasted brassicas with puy lentils & halloumi", content: "800g  broccoli and cauliflowe caraserve immediately.", image_url: "http://cdn.jamieoliver.com/recipe-database/430_575/EapG3Zqx4p19KmBDNvvdak.jpg" } )
+foodies4 = Foodie.create( { name: "Mainely Fish",	content: " 6 (3 ounce) fillets haddunwrap. One packet per person.", image_url: "http://images.media-allrecipes.com/userphotos/720x405/974614.jpg" } )
+
+
+foodies5 = Foodie.create( { name: "Too Much Chocolate Cake", content: " 1 (18.25 ounce) paar.",	image_url: "http://images.media-allrecipes.com/userphotos/720x405/1130307.jpg" } )
+foodies6 = Foodie.create( { name: "Lemon Ricotta Pancakes with Blueberry Sauce", 	content:" 2 tablee griddle and cook unter side. Ry sauce.", image_url: "http://images.media-allrecipes.com/userphotos/720x405/1538703.jpg" } )
