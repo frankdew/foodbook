8324d4599e3692a35b0b1fea511761b73689c72e
frankdew <frankdew23@gmail.com>
2016-08-06 14:38:02 +0200

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :meals
   resources :foodies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
